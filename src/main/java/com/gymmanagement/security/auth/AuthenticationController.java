2c8fa6896b4dac69c34953b627c0d666449302fd
trifan-nicusor <nicusormadalintrifan@yahoo.com>
2023-06-08 22:28:03 +0300

    Added refresh token request

--- src/main/java/com/gymmanagement/security/auth/AuthenticationController.java
+++ src/main/java/com/gymmanagement/security/auth/AuthenticationController.java
@@ -1,16 +1,19 @@
 package com.gymmanagement.security.auth;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
-public class AuthController {
+public class AuthenticationController {
 
     private final AuthenticationService authService;
 
@@ -20,7 +23,13 @@ public class AuthController {
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticateRequest request) {
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(authService.authenticate(request));
+    }
+
+    @PostMapping("/refresh-token")
+    public void refreshToken(HttpServletRequest request,
+                             HttpServletResponse response) throws IOException {
+        authService.refreshToken(request, response);
     }
 }
