e34676f6653ee9992183e48fe44ebbe23bbee7fb
trifan-nicusor <nicusormadalintrifan@yahoo.com>
2023-06-11 23:55:03 +0300

    Added functionality to EmailValidator class

--- src/main/java/com/gymmanagement/security/email/EmailValidator.java
+++ src/main/java/com/gymmanagement/security/email/EmailValidator.java
@@ -1,13 +1,18 @@
 package com.gymmanagement.security.email;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 @Service
 public class EmailValidator implements Predicate<String> {
 
+    @Value("${email-regex}")
+    private String emailRegex;
+
     @Override
     public boolean test(String email) {
-        return true;
+        return Pattern.compile(emailRegex).matcher(email).matches();
     }
 }
