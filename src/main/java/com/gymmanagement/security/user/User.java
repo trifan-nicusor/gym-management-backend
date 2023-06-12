54b9d46a0191ef35dcbe0b55a44c8dd5e6f38f18
trifan-nicusor <nicusormadalintrifan@yahoo.com>
2023-06-12 21:53:05 +0300

    Replace .* with individual imports

--- src/main/java/com/gymmanagement/security/user/User.java
+++ src/main/java/com/gymmanagement/security/user/User.java
@@ -1,7 +1,14 @@
 package com.gymmanagement.security.user;
 
 import com.gymmanagement.security.token.Token;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
