b9f106dbcad5bd1119511326c5d97139e6ba93db
trifan-nicusor <nicusormadalintrifan@yahoo.com>
2023-06-16 12:09:02 +0300

    Added Random Bean

--- src/main/java/com/gymmanagement/security/config/ApplicationConfig.java
+++ src/main/java/com/gymmanagement/security/config/ApplicationConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import java.util.Random;
 
 @Configuration
 @RequiredArgsConstructor
@@ -43,5 +44,10 @@ public class ApplicationConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public Random random() {
+        return new Random();
     }
 }
