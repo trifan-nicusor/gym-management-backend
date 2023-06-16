7d59f3d890c5f6f3352c8f8fc9ef865bbd5ea30a
trifan-nicusor <nicusormadalintrifan@yahoo.com>
2023-06-16 12:06:36 +0300

    Added PasswordRequest.java

---
+++ src/main/java/com/gymmanagement/security/auth/PasswordRequest.java
@@ -0,0 +1,15 @@
+package com.gymmanagement.security.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PasswordRequest {
+    private String currentPassword;
+    private String newPassword;
+}
