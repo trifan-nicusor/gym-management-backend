6ff0225f80d0ca457a48f339391d96b1e63a220d
trifan-nicusor <nicusormadalintrifan@yahoo.com>
2023-06-24 13:13:47 +0300

    Small code refactoring

--- src/main/java/com/gymmanagement/GymManagementApplication.java
+++ src/main/java/com/gymmanagement/GymManagementApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GymManagementApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GymManagementApplication.class, args);
     }
-
 }
