e8ea793008972941916811ea88769f9b678d8d7d
trifan-nicusor <nicusormadalintrifan@yahoo.com>
2023-06-26 19:34:00 +0300

    Added findByName method

--- src/main/java/com/gymmanagement/equipment/EquipmentRepository.java
+++ src/main/java/com/gymmanagement/equipment/EquipmentRepository.java
@@ -1,9 +1,14 @@
 package com.gymmanagement.equipment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
 
+    @Query("SELECT e FROM Equipment e WHERE e.name = ?1")
+    Optional<Equipment> findByName(String equipmentName);
 }
