9f617ce45006947dbfbddd2ee090f65d8abb168f
trifan-nicusor <nicusormadalintrifan@yahoo.com>
2023-07-03 17:31:45 +0300

    Small code refactoring

--- src/main/java/com/gymmanagement/security/token/jwt/JwtTokenRepository.java
+++ src/main/java/com/gymmanagement/security/token/jwt/JwtTokenRepository.java
@@ -3,13 +3,12 @@ package com.gymmanagement.security.token.jwt;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
 public interface JwtTokenRepository extends JpaRepository<JwtToken, Long> {
-
-    @Query("SELECT t FROM jwt_token t INNER JOIN User u ON t.user.id = u.id" +
+    @Query("SELECT t" +
+            " FROM jwt_token t INNER JOIN User u ON t.user.id = u.id" +
             " WHERE u.id = :id AND (t.isExpired = FALSE OR t.isRevoked = FALSE)")
     List<JwtToken> findAllValidTokenByUser(Long id);
 }
