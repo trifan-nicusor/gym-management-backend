0ca7cbe3784e72ec9e62fa993cf6b9bb36bf6307
trifan-nicusor <nicusormadalintrifan@yahoo.com>
2023-07-03 17:19:48 +0300

    Small code refactoring

--- src/main/java/com/gymmanagement/security/auth/AuthenticationController.java
+++ src/main/java/com/gymmanagement/security/auth/AuthenticationController.java
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Objects;
