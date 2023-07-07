42142b51f48fdadc4418b595f28025f0fc7708bb
trifan-nicusor <nicusormadalintrifan@yahoo.com>
2023-07-08 00:14:23 +0300

    Moved many to many annotation mapping to subscription entity

--- src/main/java/com/gymmanagement/discipline/Discipline.java
+++ src/main/java/com/gymmanagement/discipline/Discipline.java
@@ -1,22 +1,15 @@
 package com.gymmanagement.discipline;
 
-import com.gymmanagement.subscription.Subscription;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 @Builder
@@ -34,23 +27,4 @@ public class Discipline {
     private String longDescription;
     private boolean isActive;
     private LocalDateTime createdAt;
-    @ManyToMany(fetch = FetchType.LAZY,
-            cascade =
-                    {
-                            CascadeType.DETACH,
-                            CascadeType.MERGE,
-                            CascadeType.REFRESH,
-                            CascadeType.PERSIST
-                    },
-            targetEntity = Subscription.class)
-    @JoinTable(
-            name = "subscription_discipline",
-            joinColumns = @JoinColumn(name = "subscription_id",
-                    nullable = false,
-                    updatable = false),
-            inverseJoinColumns = @JoinColumn(name = "discipline_id",
-                    nullable = false,
-                    updatable = false)
-    )
-    private List<Subscription> subscriptionList;
 }
