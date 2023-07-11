3b895fe9227fcc1278001e9a825778ec3e0c76ce
trifan-nicusor <nicusormadalintrifan@yahoo.com>
2023-07-11 15:51:46 +0300

    Added record for add product to cart request

---
+++ src/main/java/com/gymmanagement/cart/request/AddProductRequest.java
@@ -0,0 +1,14 @@
+package com.gymmanagement.cart.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddProductRequest {
+    private Long subscriptionId;
+}
